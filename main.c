c12c19e2fce6abf5e1b06f7f7a508861fa00295d
Benjamin <baperz@miuandes.cl>
2017-10-13 18:02:49 -0300

    all algorithms working without problems including custom.

--- main.c
+++ main.c
@@ -19,13 +19,15 @@ struct disk *disk;
 const char *sel; //Selector of function to execute
 int *ft; //frame table
 int page_faults = 0;
+int *faulted_pages;
 
 void page_fault_handler( struct page_table *pt, int page )
 {
 	int nframes = page_table_get_nframes(pt);
-	printf("%d\n", page);
+	int npages = page_table_get_npages(pt);
 	page_faults++;
 	char *data = page_table_get_physmem(pt);
+
 	for (int i = 0; i < nframes; i++){
 		if(ft[i] == -1) {
 			page_table_set_entry(pt, page, i, PROT_READ|PROT_WRITE);
@@ -35,16 +37,38 @@ void page_fault_handler( struct page_table *pt, int page )
 		}
 	}
 	int selected_page = -1;
+	
 	if (strcmp("rand", sel) == 0){
+		
 		while (selected_page == -1){
 			selected_page = ft[lrand48()%nframes];
 		}
 	}
+	
 	else if (strcmp("fifo", sel) == 0){
-		selected_page = page%nframes;
+		
+		static int frame = 0; //the "first" updated frame.
+		selected_page = ft[frame];
+		frame ++;
+		frame = frame%nframes;
 	}
+	
 	else if (strcmp("custom", sel) == 0){
 		
+		faulted_pages[page] ++;
+		int lower_faults_page_index = -1;
+		int page_in_memory = 0;
+		
+		for (int i = 0; i < npages; i++){
+			for (int j = 0; j < nframes; j++){
+				if(ft[j] == i) page_in_memory = 1;
+				else page_in_memory = 0;
+			}
+			if((faulted_pages[i] <= faulted_pages[lower_faults_page_index] && page_in_memory) || (lower_faults_page_index == -1 && page_in_memory)){
+				lower_faults_page_index = i;
+			}
+		}
+		selected_page = lower_faults_page_index;
 	}
 	
 	else{
@@ -73,11 +97,16 @@ int main( int argc, char *argv[] )
 	int nframes = atoi(argv[2]);
 	sel = argv[3];
 	const char *program = argv[4];
-	
+
 	ft = malloc(nframes*sizeof(int)); //we create the frame table
 	for (int i = 0; i < nframes; i++){
 		ft[i] = -1;
 	}
+	faulted_pages = malloc(npages*sizeof(int));
+	for (int i = 0; i < npages; i++){
+		faulted_pages[i] = 0;
+	} 
+	
 	disk = disk_open("myvirtualdisk",npages);
 	if(!disk) {
 		fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
